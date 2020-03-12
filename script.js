bc6be470e469ef51baa917af7dd185550ea454eb
stii <tencktech@gmail.com>
2020-03-12 15:01:21 +0100

    finished all 10 questions

---
+++ script.js
@@ -0,0 +1,53 @@
+// Question 1
+
+var petArray = ["cat", "cow", "dog"];
+
+// Question 2
+
+console.log(petArray[1]);
+
+// Question 3
+
+petArray.push("sheep");
+
+// Question 4
+
+console.log(petArray.length);
+
+// Question 5
+
+var catObject = {
+  name: "Felix", // string
+  colour: "Black", // string
+  age: 6 // number
+};
+
+// Question 6
+
+console.log(catObject.age);
+
+// Question 7
+
+var catArray = [
+  {
+    name: "Felix",
+    colour: "Black",
+    age: 6
+  }
+];
+
+// Question 8
+
+for (var i = 0; i < catArray.length; i++) {
+  console.log(catArray[i]);
+}
+
+// Question 9
+
+function logToConsole(animal) {
+  console.log(animal);
+}
+
+// Question 10
+
+logToConsole("donkey");
